76731bc51c29306efa655acb2e581d2ddfcaf644
fuhrmann.software <fuhrmann@fuhrmann.software>
2024-11-24 11:18:08 +0100

    Game Wrapper Component intial setup

---
+++ whac-a-mole/src/app/game-wrapper/game-wrapper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-wrapper',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-wrapper.component.html',
+  styleUrl: './game-wrapper.component.scss'
+})
+export class GameWrapperComponent {
+
+}
